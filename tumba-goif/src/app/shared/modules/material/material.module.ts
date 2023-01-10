cc21fd3bda5884a5eacb76d6bf6b9dd8d375db0b
erikjoachim <erikneuhofer@gmail.com>
2023-01-10 22:59:02 +0100

    format changes

--- tumba-goif/src/app/shared/modules/material/material.module.ts
+++ tumba-goif/src/app/shared/modules/material/material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MatIconModule } from "@angular/material/icon";
 import { MatTabsModule } from "@angular/material/tabs";
+
 const materialModules = [MatIconModule, MatTabsModule];
 
 @NgModule({
